63cd25519e7390ec41bb4b7248b397cf1f9dc3a9
Amit De <amit.de@globalids.com>
2023-03-21 18:21:05 +0530

    enable gzip

--- src/main/java/uline/emma/metadataaccess/MetadataAccess.java
+++ src/main/java/uline/emma/metadataaccess/MetadataAccess.java
@@ -27,7 +27,7 @@ public class MetadataAccess {
 
     @RequestMapping(value = "/", produces = "text/html")
     String welcome() {
-        return "Welcome to EMMA metadata access application. Build 2023-03-19 17:25";
+        return "Welcome to EMMA metadata access application. Build 2023-03-21 18:20";
     }
 
     @GetMapping(value = "/search", produces = "application/json")
